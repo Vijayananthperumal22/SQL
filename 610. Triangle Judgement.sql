2cdd5a0cad57def0f1098db3507e7a7d0a2716ee
Vijay Ananth <107705127+Vijayananthperumal22@users.noreply.github.com>
2024-04-26 21:00:21 +0530

    610. Triangle Judgement.sql

---
+++ 610. Triangle Judgement.sql
@@ -0,0 +1,26 @@
+Table: Triangle
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| x           | int  |
+| y           | int  |
+| z           | int  |
++-------------+------+
+In SQL, (x, y, z) is the primary key column for this table.
+Each row of this table contains the lengths of three line segments.
+ 
+
+Report for every three line segments whether they can form a triangle.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+#code
+
+```
+SELECT * ,IF(x+y>z and y+z>x and z+x>y, "Yes","No")
+AS triangle 
+FROM Triangle;
+```
