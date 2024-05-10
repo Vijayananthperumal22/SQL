16039a279beefcf8efd734df3d59f873a9d68bf6
Vijay Ananth <107705127+Vijayananthperumal22@users.noreply.github.com>
2024-05-10 15:49:57 +0530

    Update 620. Not Boring Movies.sql

--- 620. Not Boring Movies.sql
+++ 620. Not Boring Movies.sql
@@ -44,7 +44,8 @@ Output:
 Explanation: 
 We have three movies with odd-numbered IDs: 1, 3, and 5. The movie with ID = 3 is boring so we do not include it in the answer.
 
-CODE
+CODE:
+ 
 
 select id,movie,description,rating 
 from Cinema 
